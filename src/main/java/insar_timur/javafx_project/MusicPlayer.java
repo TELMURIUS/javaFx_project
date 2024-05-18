c47c6bb61749790799ac6fc6c7f92970fe9fe9bd
askarov32 <insar@lessons.kz>
2024-05-19 01:33:57 +0500

    Modified MusicPlayer class

--- src/main/java/insar_timur/javafx_project/MusicPlayer.java
+++ src/main/java/insar_timur/javafx_project/MusicPlayer.java
@@ -2,17 +2,33 @@ package insar_timur.javafx_project;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.fxml.FXMLLoader;
 
 import java.io.File;
+import java.io.IOException;
 
 public class MusicPlayer {
 
     @FXML
     private Label chooseMusic;
+
+    @FXML
+    private Label songTitle;
+
+    @FXML
+    private Label songArtist;
+
+    @FXML
+    private ImageView albumCover;
+
     private MediaPlayer mediaPlayer;
 
     public MusicPlayer() {
@@ -24,10 +40,7 @@ public class MusicPlayer {
         fileChooser.setTitle("Select your music");
         File file = fileChooser.showOpenDialog(null);
         if (file != null) {
-            String selectedFile = file.toURI().toString();
-            Media media = new Media(selectedFile);
-            mediaPlayer = new MediaPlayer(media);
-            mediaPlayer.setOnReady(() -> chooseMusic.setText(file.getName()));
+            playSong(file.toURI().toString(), file.getName(), "", "", null);
         }
     }
 
@@ -50,5 +63,30 @@ public class MusicPlayer {
         if (mediaPlayer != null) {
             mediaPlayer.stop();
         }
+    }
+
+    @FXML
+    void goToPlaylist(MouseEvent event) throws IOException {
+        Stage stage = (Stage) chooseMusic.getScene().getWindow();
+        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("playlist-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 480);
+        stage.setScene(scene);
+    }
+
+    public void playSong(String songUri, String title, String artist, String album, String picUrl) {
+        if (mediaPlayer != null) {
+            mediaPlayer.stop();
+        }
+        Media media = new Media(songUri);
+        mediaPlayer = new MediaPlayer(media);
+        mediaPlayer.setOnReady(() -> {
+            chooseMusic.setText(title);
+            songTitle.setText(title);
+            songArtist.setText(artist);
+            if (picUrl != null) {
+                albumCover.setImage(new Image(picUrl));
+            }
+            mediaPlayer.play();
+        });
     }
 }
