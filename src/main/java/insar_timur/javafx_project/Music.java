cc981634a4417924371c07de6321931ee330a010
Telmurius <timurkassumkhanov@gmail.com>
2024-05-23 01:44:22 +0500

    WORKING GIF O MY GOSH

--- src/main/java/insar_timur/javafx_project/Music.java
+++ src/main/java/insar_timur/javafx_project/Music.java
@@ -19,37 +19,29 @@ import javafx.util.Duration;
 public class Music extends Application {
     @Override
     public void start(Stage primaryStage) {
-        // Загружаем видео (анимацию)
         Media media = new Media(getClass().getResource("/insar_timur/javafx_project/animation.gif.mp4").toExternalForm());
         MediaPlayer mediaPlayer = new MediaPlayer(media);
         MediaView mediaView = new MediaView(mediaPlayer);
 
-        // Создаем текст с названием проекта
         Text projectName = new Text("MUSIC PLAYER OF THE CENTURY");
         projectName.setFont(new Font(20));
         projectName.setFill(Color.WHITE);
 
-        // Создаем VBox для размещения MediaView и текста
         VBox vbox = new VBox(10, mediaView, projectName);
         vbox.setStyle("-fx-background-color: black;");
         vbox.setAlignment(javafx.geometry.Pos.CENTER);
 
-        // Создаем StackPane для размещения VBox
         StackPane root = new StackPane(vbox);
         root.setStyle("-fx-background-color: black;");
 
-        // Создаем сцену
         Scene scene = new Scene(root, 500, 600);
 
-        // Устанавливаем сцену на primaryStage
         primaryStage.setScene(scene);
         primaryStage.setTitle("Music Player");
         primaryStage.show();
 
-        // Начинаем воспроизведение видео
         mediaPlayer.play();
 
-        // Создаем таймлайн для отображения видео на протяжении 5 секунд
         Timeline timeline = new Timeline(
                 new KeyFrame(Duration.seconds(5), event -> {
                     // После 5 секунд переходим к основной странице
@@ -60,10 +52,8 @@ public class Music extends Application {
         timeline.play();
     }
 
-    // Метод для загрузки основной страницы
     private void loadMainPage(Stage primaryStage) {
         try {
-            // Загружаем FXML файл с основной страницей
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/insar_timur/javafx_project/music-player.fxml"));
             Scene mainScene = new Scene(fxmlLoader.load(), 400, 500);
             mainScene.getStylesheets().add(getClass().getResource("/insar_timur/javafx_project/style.css").toExternalForm());
